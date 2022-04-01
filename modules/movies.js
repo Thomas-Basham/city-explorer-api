14d9d164b489c5b6f3a4a77abb009529387d05de
Thomas Basham <Bashamtg@gmail.com>
2022-03-31 18:45:06 -0700

    formatted

--- modules/movies.js
+++ modules/movies.js
@@ -1,43 +1,38 @@
-'use strict';
-const axios = require('axios');
+"use strict";
+const axios = require("axios");
 
 // Get data from TMDB
-async function getMovies (request, response) {
-
+async function getMovies(request, response) {
   try {
+    let city = request.query.city;
 
-  let city = request.query.city;
-  
-  let url = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIE_API_KEY}&query=${city}&page=1`; //TODO change seattle to ${searchQueryCity}// HIDE API KEY
-  let movieTimes = await axios.get(url);
-    
-  let movieData = [];
-  movieTimes.data.results.forEach ((element) => {
+    let url = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIE_API_KEY}&query=${city}&page=1`; //TODO change seattle to ${searchQueryCity}// HIDE API KEY
+    let movieTimes = await axios.get(url);
+
+    let movieData = [];
+    movieTimes.data.results.forEach((element) => {
       let selectedCity = new MovieTimes(element);
-      movieData.push(selectedCity); 
+      movieData.push(selectedCity);
     });
     response.send(movieData);
 
     console.log(movieData);
-
-    } catch(error) {
-
-      // next(error); // SEND TO app.use down below
-      console.log(error);
-    }
+  } catch (error) {
+    // next(error); // SEND TO app.use down below
+    console.log(error);
   }
+}
 
 // Class
 class MovieTimes {
   constructor(element) {
-    // this.results = element.results;  
-    this.title = element.title; 
+    // this.results = element.results;
+    this.title = element.title;
     this.released = element.release_date;
     this.posterPath = element.poster_path;
-    
+
     // this.description = element.weather.description;
   }
 }
-
 
 module.exports = getMovies;
--- server.js
+++ server.js
@@ -1,56 +1,55 @@
- 'use strict';
+"use strict";
 
- // Dotenv
- require('dotenv').config();
- console.log('City Explorer Server is Running');
+// Dotenv
+require("dotenv").config();
+console.log("City Explorer Server is Running");
 
 // REQUIRE
 // Requirements for server:
 // Express takes 2 steps: 'require" and 'use.'
-const express = require('express'); 
+const express = require("express");
 const app = express();
 
 // We must include cors if we want to share resources over the web
-const cors = require('cors');
+const cors = require("cors");
 
 // Bring in Axios
-const axios = require('axios');
+const axios = require("axios");
 
-const getWeatherBit = require('./modules/weather')
-const getMovies = require('./modules/movies')
+const getWeatherBit = require("./modules/weather");
+const getMovies = require("./modules/movies");
 
 // USE
-// Once we have required something, we have to use it. This is where we assigne the required field a variable. React does this in one step with "import." 
+// Once we have required something, we have to use it. This is where we assigne the required field a variable. React does this in one step with "import."
 
 app.use(cors());
 
 // define PORT and validate .env file is working
 const PORT = process.env.PORT || 3002; // something is wrong if on 3002
 
-
 // ROUTES
 // We will write our endpoints here
 
-// Server 
-app.get('/', (request, response) => {
-  response.send('Welcome to the city explorer server!');
+// Server
+app.get("/", (request, response) => {
+  response.send("Welcome to the city explorer server!");
 });
 
 // Get our data from Weatherbit API
-app.get('/weather',getWeatherBit);
+app.get("/weather", getWeatherBit);
 
-  // GET OUR MOVIE DATA 
-app.get('/movies', getMovies);
+// GET OUR MOVIE DATA
+app.get("/movies", getMovies);
 
-  // Error
-app.get('*', (request, response) => {
-  response.send('This page does not exist');
+// Error
+app.get("*", (request, response) => {
+  response.send("This page does not exist");
 });
 
 // ERRORS
 app.use((error, request, response, next) => {
   response.status(500).send(error.message);
-}) 
+});
 
 // LISTEN
 // start the server
